15de1f8b7f853ed5c61f1c9f8ff01a5b954b20ee
Zack Patrick <zpatrick319@Hotmail.com>
2018-04-24 13:53:16 -0700

    add lb outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,14 @@ output "deploy_id" {
   value = "${layer0_deploy.mod.id}"
 }
 
+output "load_balancer_id" {
+  value = "${layer0_load_balancer.mod.id}"
+}
+
+output "load_balancer_url" {
+  value = "${layer0_load_balancer.mod.url}"
+}
+
 output "service_id" {
   value = "${layer0_service.mod.id}"
 }
